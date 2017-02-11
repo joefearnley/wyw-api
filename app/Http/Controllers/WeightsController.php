ec094a9b9598c84ddcd5eeaab036922225c988ea
Joe Fearnley <joe.fearnley@gmail.com>
2017-02-11 07:37:09 -0500

    Updating phpunit config

--- app/Http/Controllers/WeightsController.php
+++ app/Http/Controllers/WeightsController.php
@@ -6,22 +6,6 @@ use GuzzleHttp\Client;
 
 class WeightsController extends Controller
 {
-    private $fireBaseUrl;
-
-    private $fireBaseConfig;
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->fireBaseUrl = ;
-
-        $this->$fireBaseConfig = new \Geckob\Firebase\Firebase('path_to_your_secret_file.json');
-    }
-
    /**
      * Fetch all weights from the Firebase API.
      *
@@ -29,11 +13,6 @@ class WeightsController extends Controller
      */
     public function all()
     {
-        $client = new Client();
-        $response = $client->get($this->fireBaseUrl);
-
-        return response()->json(
-            json_decode($response->getBody(), true)
-        );
+        return response()->json(Weight::all());
     }
 }
